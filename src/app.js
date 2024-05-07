daf49569c6fda94bc1b70c1fcef206078d4e4013
bhoomi <bhoomi1414agarwal@gmail.com>
2024-05-07 17:12:58 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const studentRouter = require('./routes/student')
+const cors = require('cors');
+
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
